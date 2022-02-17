fa4da3899504749b84fcf9071b17478ec3538f48
ador <rashedulhaque111@gmail.com>
2022-02-18 02:23:25 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
-document.getElementById('calculate-button').addEventListener('click', function () {
+document.getElementById('calculate').addEventListener('click', function () {
     //income 
-    const myincomeInput = document.getElementById('myincome-input');
+    const myincomeInput = document.getElementById('input-income');
     const incomeAmount = myincomeInput.value;
 
+
+
     //food
-    const foodcostInput = document.getElementById('myfoodcost-input');
+    const foodcostInput = document.getElementById('food-income');
     const foodAmount = foodcostInput.value;
 
     //rentcost
-    const myrentInput = document.getElementById('rentcost-input');
+    const myrentInput = document.getElementById('rent-income');
     const rentAmount = myrentInput.value;
 
+
     //clothcost
-    const clothInput = document.getElementById('myclothcost-input');
+    const clothInput = document.getElementById('clothes-income');
     const clothAmount = clothInput.value;
 
     //total
     const expenses = parseFloat(foodAmount) + parseFloat(rentAmount) + parseFloat(clothAmount);
-    const mytotalExpenses = document.getElementById('total-expneses');
+    const mytotalExpenses = document.getElementById('Expenses-amount');
     mytotalExpenses.innerText = expenses;
 
     const balance = parseFloat(incomeAmount) - parseFloat(expenses);
-    const ourtotalbalance = document.getElementById('allbalance');
+    const ourtotalbalance = document.getElementById('Balance-amount');
     ourtotalbalance.innerText = balance;
-
 });
